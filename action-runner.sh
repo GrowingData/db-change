303651372048537042cba8b6e1e1b32b34d79b9e
Terence Siganakis <terence@growingdata.com.au>
2020-07-13 14:19:02 +1000

    Exit on error

--- action-runner.sh
+++ action-runner.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+set -e
 
 DB_CONNECTION=$1
 DB_ENVIRONMENT=$2
 PATH_DBUP=$3
-PATH_SCHEMA=$3
-GIT_BRANCH=$3
-GIT_EMAIL=$3
-GIT_NAME=$3
-GIT_NAME=$3
+PATH_SCHEMA=$4
+GIT_BRANCH=$5
+GIT_EMAIL=$6
+GIT_NAME=$7
 
 
 echo "Configuring git"
@@ -23,10 +23,10 @@ git fetch
 git checkout $GIT_BRANCH
 git pull --unshallow
 
-echo "Running db-change up..."
+echo "Running db-change up... (from ${PATH_DBUP})"
 db-change up --path "$PATH_DBUP" --engine mssql --connection "$DB_CONNECTION"
 
-echo "Generating full scripts for environment: ${DB_ENVIRONMENT}"
+echo "Generating full scripts for environment: ${DB_ENVIRONMENT} in ${$PATH_SCHEMA}"
 db-change script --path "$PATH_SCHEMA" --engine mssql --connection "$DB_CONNECTION"
 
 export CURRENT_BRANCH="$(git symbolic-ref --short HEAD)"
@@ -35,8 +35,8 @@ echo "Pushing local changes from: ${CURRENT_BRANCH}"
 git add . --all
 if ! git diff-index --quiet HEAD --; then
     echo "Changes detected, commiting and pushing..."
-    git commit -m "Database schema changes synchronized for $DB_NAME on $DB_SERVER (using environment: $DB_ENV)"
-    git push origin master
+    git commit -m "Database schema changes synchronized using environment: $DB_ENV"
+    git push origin $GIT_BRANCH
 else
     echo "No schema changes were detected"
 fi
